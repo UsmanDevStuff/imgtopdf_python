6a7c246a92f1d4860d1c10b6fa8f744f8c3f5590
Muhammad Usman <entpnr.usman@gmail.com>
2023-03-20 20:18:40 +0500

    first commit

---
+++ img_to_pdf _release.py
@@ -0,0 +1,74 @@
+import os
+import time
+from tkinter import *
+from tkinter import filedialog
+import webbrowser
+from PIL import Image
+from PyPDF2 import PdfMerger
+
+# Create the Tkinter GUI window
+root = Tk()
+root.title("Image to PDF Converter")
+root.geometry("300x140")
+
+# Define a function to convert the selected images to PDF
+# Define a function to convert the selected images to PDF
+def convert_to_pdf():
+    # Ask the user to select the input images
+    dn_label.config(text="Select images to convert")
+    filetypes = (("PNG files", "*.png"), ("JPEG files", "*.jpg"))
+    input_images = filedialog.askopenfilenames(filetypes=filetypes)
+    if not input_images:
+        return
+
+    # Ask the user to select the output PDF file
+    output_pdf = filedialog.asksaveasfilename(defaultextension=".pdf")
+    if not output_pdf:
+        return
+
+    # Create a PDF merger object
+    pdf_merger = PdfMerger()
+
+    # Loop through each input image and convert it to a PDF page
+    for input_image in input_images:
+        # Open the input image using Pillow
+        image = Image.open(input_image)
+
+        # Convert the image to RGB mode if necessary
+        if image.mode == "RGBA":
+            image = image.convert("RGB")
+        
+        dn_label.config(text="Configuring Images")
+        global temp_pdf
+        # Create a temporary PDF file for the current image
+        temp_pdf = os.path.splitext(input_image)[0] + ".pdf"
+        # Save the image to disk
+        with open(temp_pdf, "wb") as f:
+            image.save(f, "PDF", resolution=100.0)
+
+        # Add the temporary PDF file to the PDF merger
+        pdf_merger.append(temp_pdf)
+
+    # Write the merged PDF file to disk
+    with open(output_pdf, "wb") as f:
+        pdf_merger.write(f)
+    # Close the PDF merger object
+    pdf_merger.close()
+
+    # Delete the temporary PDF file
+    os.remove(temp_pdf)
+    dn_label.config(text="Pdf file saved")
+
+def callback():
+    webbrowser.open("https://github.com/entpnrusman/")
+# Add a button to trigger the conversion
+convert_button = Button(root, text="Select images to convert", command=convert_to_pdf)
+convert_button.pack(pady=10)
+
+dn_label = Label(root, text="Select images to convert")
+dn_label.pack(pady=10)
+
+github_link = Button(root, text="github.com/entpnrusman", command=callback)
+github_link.pack(pady=10)
+# Start the Tkinter main event loop
+root.mainloop()
